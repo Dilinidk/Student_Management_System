d974e1c0f140fa20542ed4fb5916cabe6733f904
Dilinidk <athapat-ps20329@stu.kln.ac.lk>
2024-11-18 08:17:33 +0530

    updateStudentRepository

--- practicalGuide_02/src/main/java/com/example/practicalGuide_02/repository/StudentRepository.java
+++ practicalGuide_02/src/main/java/com/example/practicalGuide_02/repository/StudentRepository.java
@@ -1,9 +1,23 @@
 package com.example.practicalGuide_02.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
 import com.example.practicalGuide_02.Model.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Long>{
 
+    List<Student> findByYearsOfEnrollment(int years);
+
+    @Query("SELECT department FROM Student WHERE id = ?1")
+    String findDepartmentById(long id);
+    
+    @Modifying
+    
+    @Query("DELETE FROM Student WHERE yearsOfEnrollment = ?1")
+    void deleteStudentByYearsOfEnrollment(int yearsOfEnrollment);
     
 }
